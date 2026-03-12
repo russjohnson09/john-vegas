1f97708b5c9d2fc2b79bd046e8248191b877fe58
Russell Johnson <russjohnson09@gmail.com>
2026-03-11 23:30:38 -0400

    short baby

--- python-scripts/scripts/download_baby_stocks.py
+++ python-scripts/scripts/download_baby_stocks.py
@@ -1,3 +1,9 @@
 from scripts.download_youtube_asset import download_youtube_asset
+from scripts.save_mp3_from_timing_array import save_mp3_from_timing_array
 
-download_youtube_asset("https://www.youtube.com/watch?v=lkJkPhuTCco")
+
+# create mp4 + mp3
+original_video = download_youtube_asset("https://www.youtube.com/watch?v=lkJkPhuTCco", 'short_baby')
+
+
+save_mp3_from_timing_array(original_video,timing_array)
--- python-scripts/scripts/download_youtube_asset.py
+++ python-scripts/scripts/download_youtube_asset.py
@@ -15,17 +15,19 @@ _dir = os.path.dirname(__file__)
 _assets_dir = os.path.abspath(os.path.join(_dir, '../../john-vegas/assets'))
 
 
-# PYTHONPATH=. uv run scripts/download_baby_stocks.py 
-def download_youtube_asset(url):
+
+def _mp4_to_mp3(mp4_filename, mp3_filename):
+    mp4_video = VideoFileClip(mp4_filename)
+
+    mp4_video.audio.write_audiofile(mp3_filename)
 
 
-    # print(_dir)
+def _download_mp4(url, name):
+    mp4_filename = os.path.abspath(os.path.join(_assets_dir,f'{name}.mp4'))
 
-    # os.chdir(_assets_dir)
-
-    # _john_vegas_mp4 = os.path.abspath(os.path.join(_assets_dir, "Cool Protagonist for Your Video Game (Free to Use Audio) [clnrrJOmfPk].mp4")) 
-
-    final_filename = None
+    if os.path.isfile(mp4_filename):
+        return mp4_filename
+    
 
     # def yt_dlp_monitor(self, d):
     #     final_filename  = d.get('info_dict').get('_filename')
@@ -34,10 +36,11 @@ def download_youtube_asset(url):
 
     # https://stackoverflow.com/questions/74157935/getting-the-file-name-of-downloaded-video-using-yt-dlp
     ydl_opts = {
-        "outtmpl": f'{_assets_dir}/%(uploader)s_%(title)s.%(ext)s'
+        "outtmpl": f'{_assets_dir}/{name}.%(ext)s'
         # "outtmpl": '/whatever/directory/%(uploader)s_%(title)s.%(ext)s',  # this is where you can edit how you'd like the filenames to be formatted
         # "progress_hooks": [yt_dlp_monitor]  # here's the function we just defined
         }
+
     with YoutubeDL(ydl_opts) as ydl:
         # downloaded = ydl.download(url)
         # info = ydl.extract_info(url)
@@ -48,6 +51,17 @@ def download_youtube_asset(url):
         output_filename = ydl.prepare_filename(info_dict)
         print(f"downloaded {output_filename}")
 
-    # print(final_filename)
+    return mp4_filename
 
-    pass
+
+# PYTHONPATH=. uv run scripts/download_baby_stocks.py 
+def download_youtube_asset(url, name):
+    mp4_filename = _download_mp4(url, name)
+    mp3_filename = os.path.abspath(os.path.join(_assets_dir,f'{name}.mp3'))
+
+    mp4_video = VideoFileClip(mp4_filename)
+
+    _mp4_to_mp3(mp4_filename, mp3_filename)
+
+    return mp4_video
+
